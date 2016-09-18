1813246f7e23cdbd385fe79c172b5eff8757fcee
grillazz <the@grillazz.com>
2016-09-18 12:46:45 +0200

    app added

--- ursa_rest_sqlserver/ursa_rest_sqlserver/settings.py
+++ ursa_rest_sqlserver/ursa_rest_sqlserver/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party libs
+    'rest_framework',
+    'crispy_forms',
+    'django_filters',
+    # apps
+    'core',
 ]
 
 MIDDLEWARE = [
